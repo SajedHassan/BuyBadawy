9ce21fb2a84794b83268d655427f1bf13d654f7c
SajedHassan <hassanalmorsy1959@gmail.com>
2017-09-13 19:38:37 +0300

    request (#1)
    * MultiLanguage Supported
    
    * New Values Added (ar/en)

--- app/src/main/java/com/example/user/eshtri_first_pafge/ChoosingLang.java
+++ app/src/main/java/com/example/user/eshtri_first_pafge/ChoosingLang.java
@@ -5,9 +5,13 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
+import android.content.res.Configuration;
+import android.content.res.Resources;
+import java.util.Locale;
 import android.widget.Toast;
 
 import static android.R.attr.button;
+import static android.R.attr.valueTo;
 
 /**
  * Created by user on 8/29/2017.
@@ -18,13 +22,20 @@ public class ChoosingLang extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.choosing_language);
+    final Configuration config = new Configuration(getResources().getConfiguration());
 
-        Button eng= (Button) findViewById(R.id.engB);
+        final Button eng= (Button) findViewById(R.id.engB);
         Button arab= (Button) findViewById(R.id.arabB);
         eng.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(getBaseContext(), FirstPage.class);
+        Locale locale = new Locale("en");
+                Locale.setDefault(locale);
+                    Resources res = getBaseContext().getResources();
+                Configuration config = new Configuration(res.getConfiguration());
+                config.locale = locale;
+                res.updateConfiguration(config, res.getDisplayMetrics());
                 startActivity(intent);
             }
         });
@@ -33,6 +44,13 @@ public class ChoosingLang extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                               Intent intent = new Intent(getBaseContext(), FirstPage.class);
+                Locale locale = new Locale("ar");
+                Locale.setDefault(locale);
+                Resources res = getBaseContext().getResources();
+                Configuration config = new Configuration(res.getConfiguration());
+                config.locale = locale;
+                res.updateConfiguration(config, res.getDisplayMetrics());
+
                 startActivity(intent);
             }
         });
