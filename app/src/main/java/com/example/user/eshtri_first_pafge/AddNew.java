905a3a15d8a90dcd1f63e87cc3c73b12fe10abc5
SajedHassan <hassanalmorsy1959@gmail.com>
2017-09-22 14:59:34 +0200

    Add activation and confirmation for accounts and new products

--- app/src/main/java/com/example/user/eshtri_first_pafge/AddNew.java
+++ app/src/main/java/com/example/user/eshtri_first_pafge/AddNew.java
@@ -1,33 +1,15 @@
 package com.example.user.eshtri_first_pafge;
 
 
-import android.app.AlertDialog;
-import android.content.Context;
-import android.content.res.Resources;
-import android.widget.ArrayAdapter;
-import android.widget.Button;
-import android.widget.Spinner;
-import android.widget.TextView;
-import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
 import android.view.View;
-import android.view.View.OnClickListener;
-import android.view.Menu;
-import android.app.ProgressDialog;
-import android.os.AsyncTask;
-import android.support.v7.app.ActionBarActivity;
-import android.os.Bundle;
-import android.view.MenuItem;
-import android.widget.Toast;
-
-import javax.mail.MessagingException;
+import android.widget.Button;
+import android.widget.TextView;
 
 
 public class AddNew extends AppCompatActivity {
 
-    Context context;
 
 
 
@@ -49,57 +31,29 @@ public class AddNew extends AppCompatActivity {
         add.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                new SendMail().execute("");
+                //new SendMail().execute("");
+
+                TextView name = (TextView)findViewById(R.id.name);
+                TextView details = (TextView)findViewById(R.id.details);
+                TextView properties = (TextView)findViewById(R.id.description);
+                TextView address = (TextView)findViewById(R.id.address);
+                TextView price = (TextView)findViewById(R.id.price);
+
+                String nameVal = name.getText().toString();
+                String detailsVal = details.getText().toString();
+                String propertiesVal = properties.getText().toString();
+                String addressVal = address.getText().toString();
+                String priceVal =  price.getText().toString();
+                String catVal = "1";
+
+                String task = "add";
+                ProductCDBH addNewProductThread = new ProductCDBH(AddNew.this);
+                addNewProductThread.execute(task, MainActivity.activeUser, nameVal, catVal, detailsVal, propertiesVal, addressVal, priceVal);
             }
         });
     }
 
-    private class SendMail extends AsyncTask<String, Integer, Void> {
 
-        private ProgressDialog progressDialog;
-
-        @Override
-        protected void onPreExecute() {
-            super.onPreExecute();
-            progressDialog = ProgressDialog.show(AddNew.this, "Please wait", "Sending Product Request", true, false);
-        }
-
-        @Override
-        protected void onPostExecute(Void aVoid) {
-            super.onPostExecute(aVoid);
-            progressDialog.dismiss();
-        }
-
-        protected Void doInBackground(String... params) {
-            Mail m = new Mail("Eshtery.Badawy@gmail.com", "BedouinMafia#2017");
-            String[] toArr = {"aya_aly_abouzeid@yahoo.com"};
-            m.setTo(toArr);
-            m.setFrom("Eshtery.Badawy@gmail.com");
-            m.setSubject("Eshtery Badawy | New Product Request");
-            m.setBody("Testing");
-
-            try {
-                if(m.send()) {
-//                    Toast.makeText(AddNew.this, "Request Was Sent Successfully.", Toast.LENGTH_LONG).show();
-                    display("Done", "Request Was Sent successfully.");
-                } else {
-//                    Toast.makeText(AddNew.this, "Please Check Your Internet Connection.", Toast.LENGTH_LONG).show();
-                    display("Request Failed", "Please Check Your Internet Connection.");
-
-                }
-            } catch(Exception e) {
-                Log.e("MailApp", "Could not send email", e);
-            }
-            return null;
-        }
-        public void display(String title, String message){
-            AlertDialog.Builder builder = new AlertDialog.Builder(context);
-            builder.setCancelable(true);
-            builder.setTitle(title);
-            builder.setMessage(message);
-            builder.show();
-        }
-    }
 
 
 }
