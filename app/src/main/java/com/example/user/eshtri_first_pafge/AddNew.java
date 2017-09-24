75a229d79baf24d5a6b0f989eeb8747dc9ca670b
aya aly abouzeid <aya_aly_abouzeid@yahoo.com>
2017-09-24 21:22:40 +0200

    Fix Spinner & Add Product's Photo

--- app/src/main/java/com/example/user/eshtri_first_pafge/AddNew.java
+++ app/src/main/java/com/example/user/eshtri_first_pafge/AddNew.java
@@ -4,30 +4,59 @@ package com.example.user.eshtri_first_pafge;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Spinner;
 import android.widget.TextView;
+import android.widget.Toast;
+import android.app.Activity;
+import android.content.Intent;
+import android.database.Cursor;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.provider.MediaStore;
 
 
-public class AddNew extends AppCompatActivity {
+public class AddNew extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
 
 
-
+String chosenCategory;
+    private static int RESULT_LOAD_IMAGE = 1;
+    Spinner spinner;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.add_product);
 
-        //get the spinner from the xml.
-//        Spinner dropdown = (Spinner)findViewById(R.id.catList);
-////create a list of items for the spinner.
-//        String[] items = getResources().getStringArray(R.array.categories_array);
-////create an adapter to describe how the items are displayed, adapters are used in several places in android.
-////There are multiple variations of this, but this is the basic variant.
-//        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_dropdown_item, items);
-////set the spinners adapter to the previously created one.
-//        dropdown.setAdapter(adapter);
+         spinner = (Spinner) findViewById(R.id.catList);
+        spinner.setOnItemSelectedListener(this);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+                R.array.categories, android.R.layout.simple_spinner_item);
+
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+        spinner.setAdapter(adapter);
+
         Button add = (Button) findViewById(R.id.button_submit);
+        Button addPhoto = (Button) findViewById(R.id.AddPhoto);
+
+
+        addPhoto.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View arg0) {
+
+                Intent i = new Intent(
+                        Intent.ACTION_PICK,
+                        android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+
+                startActivityForResult(i, RESULT_LOAD_IMAGE);
+            }
+        });
+
         add.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -54,9 +83,36 @@ public class AddNew extends AppCompatActivity {
     }
 
 
+    @Override
+    public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+        chosenCategory = adapterView.getItemAtPosition(i).toString();
+    }
 
+    @Override
+    public void onNothingSelected(AdapterView<?> adapterView) {
 
-}
+    }
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == RESULT_LOAD_IMAGE && resultCode == RESULT_OK && null != data) {
+            Uri selectedImage = data.getData();
+            String[] filePathColumn = { MediaStore.Images.Media.DATA };
+
+            Cursor cursor = getContentResolver().query(selectedImage,
+                    filePathColumn, null, null, null);
+            cursor.moveToFirst();
+
+            int columnIndex = cursor.getColumnIndex(filePathColumn[0]);
+            String picturePath = cursor.getString(columnIndex);
+            cursor.close();
+
+            ImageView imageView = (ImageView) findViewById(R.id.imgView);
+            imageView.setImageBitmap(BitmapFactory.decodeFile(picturePath));
+
+        }
+}}
+
 
 
 
